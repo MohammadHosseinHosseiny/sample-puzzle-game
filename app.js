99692383745eaf0acc92af60680a0cc1ecf1918d
hosen agha <smhhb.250@gmail.com>
2024-06-10 16:58:35 +0330

    vertical version com 1

---
+++ app.js
@@ -0,0 +1,90 @@
+
+let dragItemContainer = document.querySelector(".dragItemContainer")
+let dragItemPlace = document.querySelector(".downPlace")
+let checkBTN = document.querySelector(".checkBTN")
+let resetbtn =document.querySelector(".reset")
+let generateBtn =document.querySelector(".reGenerate")
+
+let items =[]
+
+generateNewItem()
+
+dragItemPlace.addEventListener("drop",event=>{
+    let ite=event.dataTransfer.getData("itemId")
+    dragItemPlace.append(document.getElementById(ite))
+})
+
+dragItemPlace.addEventListener("dragover",event=>{
+event.preventDefault()
+})
+
+function chekResualt(){
+    if(dragItemPlace.children.length == 4){
+    let userAnswer=[]
+    for(let i=0;i<dragItemPlace.children.length;i++){
+        userAnswer.push(Number(dragItemPlace.children[i].innerHTML))
+    }
+    console.log(userAnswer)
+let answer = sortNumbers(...userAnswer)
+console.log(answer)
+let help =-1
+let res = userAnswer.every(num=>{
+    help++
+    return num == answer[help]
+})
+
+if(res){
+    alert("cool your solve puzzle")
+}else alert("your wrong try again")
+    }else alert("your not complete puzzle")
+}
+
+function sortNumbers(...numbers){
+
+for(let j=0;j < numbers.length;j++){
+for(let i=0;i < numbers.length-1;i++){
+if(numbers[i]>numbers[i+1]){
+    numbers.splice(i,2,numbers[i+1],numbers[i])
+}
+}
+}
+return numbers
+}
+
+checkBTN.addEventListener("click",chekResualt)
+
+function reset(){
+    // if(dragItemPlace.children.length == 4)
+   dragItemPlace.innerHTML=""
+   items.forEach(item=>{
+       dragItemContainer.append(item)
+   })
+//  alert("your not have any move yet")
+}
+
+resetbtn.addEventListener("click",reset)
+
+
+generateBtn.addEventListener("click",generateNewItem)
+
+
+
+function generateNewItem(){
+    reset()
+    items=[]
+    dragItemContainer.innerHTML=""
+    for(let i =0;i<4;i++){
+   let li = document.createElement("li")
+   li.classList.add("drg")
+   li.draggable="true"
+   li.innerHTML= Math.floor(Math.random()*99)
+   li.id= li.innerHTML
+   li.addEventListener("dragstart",event=>{
+       event.dataTransfer.setData("itemId",event.target.id)
+   })
+   items.push(li)
+   dragItemContainer.append(li)
+    }
+
+}
+
